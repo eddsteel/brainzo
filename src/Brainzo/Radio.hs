ba899da1d73823007548b44422e9c4ccc5236ce0
Edd Steel <edward.steel@gmail.com>
2016-01-30 18:28:11 -0800

    refactor, add radio kees, better radio np

--- src/Brainzo/Radio.hs
+++ src/Brainzo/Radio.hs
@@ -7,8 +7,9 @@ import Data.Maybe(fromMaybe)
 import Data.Map(Map)
 import qualified Data.Map as Map
 
-type Config   = String
-type Stations = Map String String
+type Config    = String
+type Stations  = Map String String
+data Direction = Bwd | Fwd
 
 npfile         :: String
 npfile          = "~/.radio-np"
@@ -20,6 +21,7 @@ radio                               :: Maybe Config -> [String] -> (Operation, [
 radio (Just c) ("list":rest)         = (list c, rest)
 radio (Just c) ("play":station:rest) = (playByKey c station, rest)
 radio (Just c) ("seek":rest)         = (seek c, rest)
+radio (Just c) ("kees":rest)         = (kees c, rest)
 radio _        ("off":rest)          = (off, rest)
 radio _        ("np":rest)           = (np, rest)
 radio Nothing  all                   = (err "radio needs some stations.", all)
@@ -28,36 +30,42 @@ radio _        (op:rest)             = (err ("radio doesn't understand " ++ op +
 off            :: Operation
 off             = cat [run ["killall", "mplayer"], run ["rm", "-f", npfile]]
 
-play           :: String -> Operation
-play url        = cat [ off
-                      , writeO npfile url
+play           :: String -> String -> Operation
+play key url    = cat [ off
+                      , writeO npfile key
                       , run ["mplayer", "-really-quiet", url]]
 
 playByKey      :: Config -> String -> Operation
-playByKey c s   = orNoop (play <$> specified)
+playByKey c s   = orNoop (play s <$> specified c)
   where
-    specified = Map.lookup s (parseStations c)
+    specified = withStations (Map.lookup s)
 
-playNext       :: Config -> String -> Operation
-playNext c stn  = play next
+playNext       :: Direction -> Config -> String -> Operation
+playNext d c key  = (uncurry play) (next c)
   where
-    stns = parseStations c
-    streams = concat . repeat . (fmap snd) . Map.toList $ stns
-    next = head (tail (dropWhile (/= stn) streams))
+    streams Fwd = concat . repeat . Map.toList . parseStations
+    streams Bwd = concat . repeat . reverse . Map.toList . parseStations
+    second = head . tail
+    next c = second (dropWhile ((/= key) . fst) (streams d c))
 
 playFirst      :: Config -> Operation
-playFirst c     = play first
-  where first = (snd . head . Map.toList) $  parseStations c
+playFirst c     = (uncurry play) first
+  where first = withStations (head . Map.toList) c
 
 list           :: Config -> Operation
-list c          = say $ (unlines . Map.keys) $ parseStations c
+list            = withStations (\stns -> say $ (unlines . Map.keys) stns)
 
 np             :: Operation
 np              = readO npfile say (say "off")
 
 seek           :: Config -> Operation
-seek c          = readO npfile (playNext c) (playFirst c)
+seek c          = readO npfile (playNext Fwd c) (playFirst c)
 
+kees           :: Config -> Operation
+kees c          = readO npfile (playNext Bwd c) (playFirst c)
+
+withStations   :: (Stations -> a) -> Config -> a
+withStations f  = f . parseStations
 
 parseStations  :: Config -> Stations
 parseStations s = Map.fromList $ foldr tupleOrDrop [] (words <$> lines s)
